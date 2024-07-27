9207035bc26dba7b46610cfedd11eaccad14821e
vasimali8 <101465941+vasimali8@users.noreply.github.com>
2024-07-27 16:00:47 +0530

    Fix ESLint error in Editor.jsx by removing unused Controlled import

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,7 +6,7 @@ import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
-import { Controlled, Controlled as ControlledEditor } from 'react-codemirror2';
+import {  Controlled as ControlledEditor } from 'react-codemirror2';
 import{Box , styled} from "@mui/material";
 import { CloseFullscreen } from '@mui/icons-material';
 
